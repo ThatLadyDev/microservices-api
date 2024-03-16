57c1f7404992bcdac279285958f7c6fadf130198
Lois Bassey <loisbassey@gmail.com>
2024-03-16 02:15:15 +0100

    created an action class for handling tasks perform logic

---
+++ app/Actions/PerformTasks.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Actions;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class PerformTasks
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+
+    public function asController()
+    {
+        
+    }
+}
