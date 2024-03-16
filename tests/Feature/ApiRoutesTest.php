f601c313818b6478e6c6396febe79d72899958c5
Lois Bassey <loisbassey@gmail.com>
2024-03-16 02:13:32 +0100

    pest setup

---
+++ tests/Feature/ApiRoutesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+test('Route exist', function(){
+    expect('3')->toBe('3');
+});
+
+// class ApiRoutesTest extends TestCase
+// {
+    
+// }
