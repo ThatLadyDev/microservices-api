1a6d0c52fd755ad4528fb278ddc4b3c1b44f2252
Lois Bassey <loisbassey@gmail.com>
2024-03-19 19:41:10 +0100

    test refactoring

--- tests/Feature/API/Functionalities/PostEndpointTest.php
+++ tests/Feature/API/Functionalities/PostEndpointTest.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Enums\TasksEnum;
-use App\Models\ProcessedJob;
 use App\Jobs\ProcessTasksJob;
-use App\Models\ProcessedTask;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -22,48 +20,44 @@ test('that mock result action is added to queue', function () {
 
     $response = $this->postJson('/api/perform-tasks', $requestData);
 
+    // Assert that the TestJob was dispatched to the queue
+    Queue::assertPushed(ProcessTasksJob::class);
+
     $response->assertStatus(200)->assertValid()->assertJsonFragment([
         'success' => true,
         'message' => 'Mock result action added to a queue'
     ]);
-
-    // Assert that the TestJob was dispatched to the queue
-    Queue::assertPushed(ProcessTasksJob::class);
 });
 
-// Green Phase
+// Refactor Phase
 test('that processed task job is stored in the database', function () {
-    $jobId = 50;
+    $jobId = null;
+    Queue::fake(); // Fake the queue
+
     $tasks = TasksEnum::toArray();
-    $taskData = [
+
+    $requestData = [
         'text' => $this->faker->word,
         'tasks' => [Arr::random($tasks)],
     ];
 
-    $actions = [
-        'call_reason' => ['Log reason', 'Document purpose', 'Note reason'],
-        'call_actions' => ['Assign tasks', 'Note actions', 'Schedule follow-ups'],
-        'satisfaction' => ['Send survey', 'Confirm satisfaction', 'Log feedback'],
-        'call_segments' => ['Summarize points', 'Segment notes', 'Task segments'],
-        'summary' => ['Document overview', 'Brief summary', 'Capture highlights']
-    ];
+    $response = $this->postJson('/api/perform-tasks', $requestData);
 
-    foreach ($taskData['tasks'] as $task) {
+    // Assert that the TestJob was dispatched to the queue
+    Queue::assertPushed(ProcessTasksJob::class);
+
+    foreach ($requestData['tasks'] as $task) {
         # code...
-        $taskUuid = Str::uuid();
-
-        ProcessedJob::query()->create([
-            'uuid' => Str::uuid(),
-            'type' => 'task',
-            'job_id' => $jobId,
-            'metadata' => ['task-uuid' => $taskUuid]
-        ]);
-
-        ProcessedTask::query()->create([
-            'uuid' => $taskUuid,
+        $this->assertDatabaseHas('processed_tasks', [
+            'text' => $requestData['text'],
             'title' => $task,
-            'text' => $taskData['text'],
-            'action' => Arr::random($actions[$task]),
         ]);
     }
+
+    $response->assertStatus(200)
+    ->assertValid()
+    ->assertJsonFragment([
+        'success' => true,
+        'message' => 'Mock result action added to a queue'
+    ]);
 });
