18885fc86a86dde11d0a8ae12e2d186783abf7cb
Lois Bassey <loisbassey@gmail.com>
2024-03-19 19:36:05 +0100

    logic addition: process tasks job

--- app/Jobs/ProcessTasksJob.php
+++ app/Jobs/ProcessTasksJob.php
@@ -2,27 +2,28 @@
 
 namespace App\Jobs;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use App\Models\ProcessedJob;
+use App\Models\ProcessedTask;
 use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
 class ProcessTasksJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $jobId;
-
-    public $resultAction;
     public $taskData;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(string $resultAction, array $taskData = [])
+    public function __construct(array $taskData = [])
     {
-        $this->resultAction = $resultAction;
         $this->taskData = $taskData;
     }
 
@@ -31,7 +32,44 @@ class ProcessTasksJob implements ShouldQueue
      */
     public function handle(): void
     {
-        logger($this->resultAction);
         $this->jobId = $this->job->getJobId();
+
+        foreach ($this->taskData['tasks'] as $task) {
+            # code...
+            $taskUuid = Str::uuid();
+
+            ProcessedJob::query()->create([
+                'uuid' => Str::uuid(),
+                'type' => 'task',
+                'job_id' => $this->jobId,
+                'metadata' => ['task-uuid' => $taskUuid]
+            ]);
+
+            ProcessedTask::query()->create([
+                'uuid' => $taskUuid,
+                'title' => $task,
+                'text' => $this->taskData['text'],
+                'action' => $this->fetchMockResultAction($task),
+                'is_queued' => true,
+            ]);
+        }
+    }
+
+    /**
+     * Summary of fetchMockResultAction
+     * @param string $task
+     * @return mixed
+     */
+    private function fetchMockResultAction(string $task)
+    {
+        $actions = [
+            'call_reason' => ['Log reason', 'Document purpose', 'Note reason'],
+            'call_actions' => ['Assign tasks', 'Note actions', 'Schedule follow-ups'],
+            'satisfaction' => ['Send survey', 'Confirm satisfaction', 'Log feedback'],
+            'call_segments' => ['Summarize points', 'Segment notes', 'Task segments'],
+            'summary' => ['Document overview', 'Brief summary', 'Capture highlights']
+        ];
+
+        return Arr::random($actions[$task]);
     }
 }
