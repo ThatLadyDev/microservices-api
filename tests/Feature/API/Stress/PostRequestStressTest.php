49f1a81bd9c1d98118ddf04c9f11883a323dd96d
Lois Onyinyemme Bassey <loisbassey@gmail.com>
2024-03-20 11:34:12 +0100

    stress test file creation

---
+++ tests/Feature/API/Stress/PostRequestStressTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Enums\TasksEnum;
+use Illuminate\Support\Arr;
+use function Pest\Stressless\stress;
+
+it('handles concurrent POST requests : {route-here}', function () {
+//    $tasks = TasksEnum::toArray();
+//    $requestData = [
+//        'text' => 'test-test',
+//        'tasks' => [Arr::random($tasks)],
+//    ];
+//
+//    $result = stress(config('app.url') . '/api/perform-tasks')
+//        ->post($requestData)
+//        ->concurrently(requests: 2)->for(5)->seconds();
+//
+//    expect($result->requests->duration->med)
+//        ->toBeLessThan(6);
+});
+
+it('handles concurrent POST requests', function () {
+
+});
+
+it('handles mixed GET and POST requests', function () {
+
+});
+
+//test('example', function () {
+//    $response = $this->get('/');
+//
+//    $response->assertStatus(200);
+//});
